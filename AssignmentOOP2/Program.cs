829fffa1e5103d3055539cb35cec3f7106d3ce24
abdalla mohamed <abdallamohamed56666.com>
2024-07-19 23:42:59 +0300

    Inheritance

--- AssignmentOOP2/Program.cs
+++ AssignmentOOP2/Program.cs
@@ -38,6 +38,16 @@
             Console.WriteLine(Note["Karim"]);
 
             Console.WriteLine(Note[0]);
+
+            /////////////////////////////////////
+            Parent P01 = new Parent(3, 5);
+            Console.WriteLine(P01.ToString());
+            Console.WriteLine(P01.Product());
+
+            Child C01 = new Child(4, 6, 7);
+            Console.WriteLine(C01.ToString());
+            Console.WriteLine(C01.Product());
+
         }
     }
 }
